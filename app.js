68943ec82388db52da30981fd8496c3d14575945
Cesalie <uwimpuhweeliphaz@gmail.com>
2025-11-17 18:00:37 +0200

    WEB-14: Add homepage route with welcome message

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
 
+// Home route
 app.get('/', (req, res) => {
     res.send("Home Page");
 });
 
+// New features route
+app.get('/new', (req, res) => {
+    res.send("New features Welcome");
+});
 
+// Start the server
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
-app.get('/', (req, res) => {
-    res.send("New features Welcom");
-});
+
