689a96292a87fbce598956adffb951de818b6194
Cesalie <uwimpuhweeliphaz@gmail.com>
2025-11-17 18:28:35 +0200

    WEB-14 <message>

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.get('/new', (req, res) => {
 
 // Start the server
 app.listen(3000, () => {
-    console.log("Server running on port 3000");
+    console.log("Server running on ports 3000");
 });
 
